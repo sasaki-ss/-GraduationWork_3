5c0e8bf79da7e8e16f0325b3d0a6ec1475b0d493
dooooori <binamu1215@gmail.com>
2021-11-22 17:54:37 +0900

    [update] Playerが壁にめり込まないように更新

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //プレイヤーのコンポーネント
     SpriteRenderer sr;
     Rigidbody2D rb;
     Animator anim;
 
+    //ステータス
     private float speed;
     private float jumpPower;
-
-    private const int MaxJumpCount = 2;
     private int jumpCount;
 
+    //定数
+    private const int MaxJumpCount = 2;
+
+    //フレームカウント
     private int count;
+
+    //壁判定
+    GameObject[] _wallContact;
+    WallContact[] scr_WallContact;
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
@@ -26,6 +34,13 @@ public class Player : MonoBehaviour
         jumpCount = 0;
 
         count = 0;
+
+        _wallContact = new GameObject[2];
+        scr_WallContact = new WallContact[2];
+        _wallContact[0] = transform.Find("Contact_L").gameObject;
+        _wallContact[1] = transform.Find("Contact_R").gameObject;
+        scr_WallContact[0] = _wallContact[0].GetComponent<WallContact>();
+        scr_WallContact[1] = _wallContact[1].GetComponent<WallContact>();
     }
 
     // Update is called once per frame
@@ -39,21 +54,19 @@ public class Player : MonoBehaviour
 
     void Walk()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && !scr_WallContact[0].getContact)
         {   //左移動
             transform.position -= new Vector3(speed, 0, 0);
             sr.flipX = true;
             anim.SetFloat("Speed", speed);
         }
-        if(Input.GetKeyUp(KeyCode.A)) anim.SetFloat("Speed", -1);
-
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D) && !scr_WallContact[1].getContact)
         {   //右移動
             transform.position += new Vector3(speed, 0, 0);
             sr.flipX = false;
             anim.SetFloat("Speed", speed);
         }
-        if (Input.GetKeyUp(KeyCode.D)) anim.SetFloat("Speed", -1);
+        else anim.SetFloat("Speed", -1);
     }
     void Jump()
     {
